23c87611a0a0c5e6dc69b3ef8c3318f16e1d9190
Osamu TONOMORI <osamingo@gmail.com>
2019-04-05 23:49:59 +0900

    Implement luhn provider

--- luhn.go
+++ luhn.go
@@ -4,10 +4,38 @@ type luhn struct{}
 
 // Verify implements checkdigit.Verifier interface.
 func (l *luhn) Verify(code string) bool {
-	return false
+
+	if len(code) < 2 {
+		return false
+	}
+
+	i, err := l.Generate(code[:len(code)-1])
+
+	return err == nil && i == int(code[len(code)-1]-'0')
+
 }
 
 // Generate implements checkdigit.Generator interface.
-func (l *luhn) Generate(seed string) (rune, error) {
-	return 0, nil
+func (l *luhn) Generate(seed string) (int, error) {
+
+	if seed == "" {
+		return 0, ErrInvalidArgument
+	}
+
+	sum, parity := 0, (len(seed)+1)%2
+	for i, n := range []rune(seed) {
+		if n < '0' || '9' < n {
+			return 0, ErrInvalidArgument
+		}
+		d := int(n - '0')
+		if i%2 == parity {
+			d *= 2
+			if d > 9 {
+				d -= 9
+			}
+		}
+		sum += d
+	}
+
+	return sum * 9 % 10, nil
 }
--- luhn_test.go
+++ luhn_test.go
@@ -4,8 +4,94 @@ import "testing"
 
 func TestLuhn_Verify(t *testing.T) {
 
+	cases := map[string]struct {
+		in  string
+		out bool
+	}{
+		"Regular 1": {
+			in:  "4242424242424242",
+			out: true,
+		},
+		"Regular 2": {
+			in:  "5105105105105100",
+			out: true,
+		},
+		"Regular 3": {
+			in:  "34",
+			out: true,
+		},
+		"Irregular 1": {
+			in: "510510510510511",
+		},
+		"Irregular 2": {
+			in: "",
+		},
+		"Irregular 3": {
+			in: "a",
+		},
+	}
+
+	for name, c := range cases {
+		c := c
+		t.Run(name, func(t *testing.T) {
+			ret := NewLuhnProvider().Verify(c.in)
+			if c.out != ret {
+				t.Errorf("not equal, expected = %v, given = %v", c.out, ret)
+			}
+		})
+	}
 }
 
 func TestLuhn_Generate(t *testing.T) {
 
+	cases := map[string]struct {
+		in      string
+		out     int
+		isError bool
+	}{
+		"Regular 1": {
+			in:  "424242424242424",
+			out: 2,
+		},
+		"Regular 2": {
+			in:  "510510510510510",
+			out: 0,
+		},
+		"Regular 3": {
+			in:  "37144963539843",
+			out: 1,
+		},
+		"Regular 4": {
+			in:  "3056930902590",
+			out: 4,
+		},
+		"Regular 5": {
+			in:  "353011133330000",
+			out: 0,
+		},
+		"Regular 6": {
+			in:  "3",
+			out: 4,
+		},
+		"Irregular 1": {
+			isError: true,
+		},
+		"Irregular 2": {
+			in:      "a",
+			isError: true,
+		},
+	}
+
+	for name, c := range cases {
+		c := c
+		t.Run(name, func(t *testing.T) {
+			r, err := NewLuhnProvider().Generate(c.in)
+			if c.isError && err == nil {
+				t.Error("unexpected error")
+			}
+			if c.out != r {
+				t.Errorf("not equal, expected = %c, given = %c", c.out, r)
+			}
+		})
+	}
 }
