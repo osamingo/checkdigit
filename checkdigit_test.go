26e4a22074c0ca46924dc6bc7d61a31f5892d175
Osamu TONOMORI <osamingo@gmail.com>
2019-04-06 00:29:51 +0900

    Add NewProdivers func testing

--- checkdigit_test.go
+++ checkdigit_test.go
@@ -4,12 +4,24 @@ import "testing"
 
 func TestNewLuhnProvider(t *testing.T) {
 
+	p := NewLuhnProvider()
+	if p == nil {
+		t.Error("failed to generate luhn provider")
+	}
 }
 
 func TestNewDammProvider(t *testing.T) {
 
+	p := NewDammProvider()
+	if p == nil {
+		t.Error("failed to generate damm provider")
+	}
 }
 
 func TestNewVerhoeffProvider(t *testing.T) {
 
+	p := NewVerhoeffProvider()
+	if p == nil {
+		t.Error("failed to generate verhoeff provider")
+	}
 }
