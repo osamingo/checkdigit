69c4ec84dc116e010f206e528fbcd7d94d3c8a23
Osamu TONOMORI <osamingo@gmail.com>
2019-04-06 03:13:50 +0900

    Implement Verhoeff provider

--- verhoeff.go
+++ verhoeff.go
@@ -8,10 +8,30 @@ type verhoeff struct {
 
 // Verify implements checkdigit.Verifier interface.
 func (v *verhoeff) Verify(code string) bool {
-	return false
+
+	if len(code) < 2 {
+		return false
+	}
+
+	i, err := v.Generate(code[:len(code)-1])
+
+	return err == nil && i == int(code[len(code)-1]-'0')
 }
 
 // Generate implements checkdigit.Generator interface.
 func (v *verhoeff) Generate(seed string) (int, error) {
-	return 0, nil
+
+	if seed == "" {
+		return 0, ErrInvalidArgument
+	}
+
+	interim, l := 0, len(seed)
+	for i, n := range []rune(seed) {
+		if n < '0' || '9' < n {
+			return 0, ErrInvalidArgument
+		}
+		interim = v.multiplication[interim][v.permutation[(i+1)%8][seed[l-i-1]-'0']]
+	}
+
+	return v.inverse[interim], nil
 }
--- verhoeff_test.go
+++ verhoeff_test.go
@@ -4,8 +4,90 @@ import "testing"
 
 func TestVerhoeff_Verify(t *testing.T) {
 
+	cases := map[string]struct {
+		in  string
+		out bool
+	}{
+		"Regular 1": {
+			in:  "938472210036",
+			out: true,
+		},
+		"Regular 2": {
+			in:  "0973652",
+			out: true,
+		},
+		"Regular 3": {
+			in:  "27",
+			out: true,
+		},
+		"Irregular 1": {
+			in: "2361",
+		},
+		"Irregular 2": {
+			in: "",
+		},
+		"Irregular 3": {
+			in: "a",
+		},
+	}
+
+	for name, c := range cases {
+		c := c
+		t.Run(name, func(t *testing.T) {
+			ret := NewVerhoeffProvider().Verify(c.in)
+			if c.out != ret {
+				t.Errorf("not equal, expected = %v, given = %v", c.out, ret)
+			}
+		})
+	}
 }
 
 func TestVerhoeff_Generate(t *testing.T) {
 
+	cases := map[string]struct {
+		in      string
+		out     int
+		isError bool
+	}{
+		"Regular 1": {
+			in:  "236",
+			out: 3,
+		},
+		"Regular 2": {
+			in:  "097365",
+			out: 2,
+		},
+		"Regular 3": {
+			in:  "93847221003",
+			out: 6,
+		},
+		"Regular 4": {
+			in:  "2",
+			out: 7,
+		},
+		"Irregular 1": {
+			isError: true,
+		},
+		"Irregular 2": {
+			in:      "a",
+			isError: true,
+		},
+		"Irregular 3": {
+			in:      "a",
+			isError: true,
+		},
+	}
+
+	for name, c := range cases {
+		c := c
+		t.Run(name, func(t *testing.T) {
+			r, err := NewVerhoeffProvider().Generate(c.in)
+			if c.isError && err == nil {
+				t.Error("unexpected error")
+			}
+			if c.out != r {
+				t.Errorf("not equal, expected = %d, given = %d", c.out, r)
+			}
+		})
+	}
 }
