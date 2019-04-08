3f2c4ed5001aeee76d686e03261ddcbe06cb4bd7
Osamu TONOMORI <osamingo@gmail.com>
2019-04-08 21:57:18 +0900

    Add examples

--- checkdigit_test.go
+++ checkdigit_test.go
@@ -49,6 +49,62 @@ func TestNewISBN13(t *testing.T) {
 	}
 }
 
+func TestNewEAN8(t *testing.T) {
+
+	p := checkdigit.NewEAN8()
+	if p == nil {
+		t.Error("failed to generate ean8 provider")
+	}
+}
+
+func TestNewEAN13(t *testing.T) {
+
+	p := checkdigit.NewEAN13()
+	if p == nil {
+		t.Error("failed to generate ean13 provider")
+	}
+}
+
+func TestNewJAN8(t *testing.T) {
+
+	p := checkdigit.NewJAN8()
+	if p == nil {
+		t.Error("failed to generate jan8 provider")
+	}
+}
+
+func TestNewJAN13(t *testing.T) {
+
+	p := checkdigit.NewJAN13()
+	if p == nil {
+		t.Error("failed to generate jan13 provider")
+	}
+}
+
+func TestNewITF(t *testing.T) {
+
+	p := checkdigit.NewITF()
+	if p == nil {
+		t.Error("failed to generate itf provider")
+	}
+}
+
+func TestNewUPC(t *testing.T) {
+
+	p := checkdigit.NewUPC()
+	if p == nil {
+		t.Error("failed to generate upc provider")
+	}
+}
+
+func TestNewSSCC(t *testing.T) {
+
+	p := checkdigit.NewSSCC()
+	if p == nil {
+		t.Error("failed to generate sscc provider")
+	}
+}
+
 func ExampleNewLuhn() {
 
 	p := checkdigit.NewLuhn()
@@ -127,4 +183,123 @@ func ExampleNewISBN13() {
 
 	// Output:
 	// seed: 978000271217, check digit: 0, verify: true
+}
+
+func ExampleNewEAN8() {
+
+	p := checkdigit.NewEAN8()
+
+	seed := "9638507"
+	cd, err := p.Generate(seed)
+	if err != nil {
+		log.Fatalln("failed to generate check digit")
+	}
+
+	ok := p.Verify(seed + strconv.Itoa(cd))
+	fmt.Printf("seed: %s, check digit: %d, verify: %t\n", seed, cd, ok)
+
+	// Output:
+	// seed: 9638507, check digit: 4, verify: true
+}
+
+func ExampleNewEAN13() {
+
+	p := checkdigit.NewEAN13()
+
+	seed := "590123412345"
+	cd, err := p.Generate(seed)
+	if err != nil {
+		log.Fatalln("failed to generate check digit")
+	}
+
+	ok := p.Verify(seed + strconv.Itoa(cd))
+	fmt.Printf("seed: %s, check digit: %d, verify: %t\n", seed, cd, ok)
+
+	// Output:
+	// seed: 590123412345, check digit: 7, verify: true
+}
+
+func ExampleNewJAN8() {
+
+	p := checkdigit.NewJAN8()
+
+	seed := "4996871"
+	cd, err := p.Generate(seed)
+	if err != nil {
+		log.Fatalln("failed to generate check digit")
+	}
+
+	ok := p.Verify(seed + strconv.Itoa(cd))
+	fmt.Printf("seed: %s, check digit: %d, verify: %t\n", seed, cd, ok)
+
+	// Output:
+	// seed: 4996871, check digit: 2, verify: true
+}
+
+func ExampleNewJAN13() {
+
+	p := checkdigit.NewJAN13()
+
+	seed := "456995111617"
+	cd, err := p.Generate(seed)
+	if err != nil {
+		log.Fatalln("failed to generate check digit")
+	}
+
+	ok := p.Verify(seed + strconv.Itoa(cd))
+	fmt.Printf("seed: %s, check digit: %d, verify: %t\n", seed, cd, ok)
+
+	// Output:
+	// seed: 456995111617, check digit: 9, verify: true
+}
+
+func ExampleNewITF() {
+
+	p := checkdigit.NewITF()
+
+	seed := "1456995111617"
+	cd, err := p.Generate(seed)
+	if err != nil {
+		log.Fatalln("failed to generate check digit")
+	}
+
+	ok := p.Verify(seed + strconv.Itoa(cd))
+	fmt.Printf("seed: %s, check digit: %d, verify: %t\n", seed, cd, ok)
+
+	// Output:
+	// seed: 1456995111617, check digit: 6, verify: true
+}
+
+func ExampleNewUPC() {
+
+	p := checkdigit.NewUPC()
+
+	seed := "01234567890"
+	cd, err := p.Generate(seed)
+	if err != nil {
+		log.Fatalln("failed to generate check digit")
+	}
+
+	ok := p.Verify(seed + strconv.Itoa(cd))
+	fmt.Printf("seed: %s, check digit: %d, verify: %t\n", seed, cd, ok)
+
+	// Output:
+	// seed: 01234567890, check digit: 5, verify: true
+}
+
+func ExampleNewSSCC() {
+
+	p := checkdigit.NewSSCC()
+
+	seed := "04569951110000001"
+	cd, err := p.Generate(seed)
+	if err != nil {
+		log.Fatalln("failed to generate check digit")
+	}
+
+	ok := p.Verify(seed + strconv.Itoa(cd))
+	fmt.Printf("seed: %s, check digit: %d, verify: %t\n", seed, cd, ok)
+
+	// Output:
+	// seed: 04569951110000001, check digit: 6, verify: true
 }
